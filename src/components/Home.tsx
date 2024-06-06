7c646c80a574af9530cd864e17de47df44682b1d
BryanGordon <bryanbge92@gmail.com>
2024-06-05 19:02:53 -0500

    Add api types

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,9 @@
 import { Navbar } from './Navbar'
+import { type Characters, res } from '../types/api'
 
 export function Home () {
+  const data: Characters = res
+  console.log('component   ' + res)
   return (
     <section>
       <header>
@@ -12,14 +15,14 @@ export function Home () {
         <article className='player-card'>
 
           <picture>
-            <img src='img.com' alt='imagen-personaje' />
+            <img src='' alt={data.name} />
           </picture>
 
           <div className='pers-info-container'>
-            <h4>Nombre del personaje</h4>
-            <span className='status-zone'>Status</span>
-            <h5>Especie</h5>
-            <h5>Genero</h5>
+            <h4>{data.name}</h4>
+            <span className='status-zone'>{data.status}</span>
+            <h5>{data.species}</h5>
+            <h5>{data.gender}</h5>
           </div>
 
         </article>
