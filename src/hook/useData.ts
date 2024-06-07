6c80307f80103830b0fb16120ed3c17c6b6bbbf0
BryanGordon <bryanbge92@gmail.com>
2024-06-07 15:05:20 -0500

    Add locations api request

--- src/hook/useData.ts
+++ src/hook/useData.ts
@@ -1,13 +1,17 @@
-import { RickMortyCharacters, type Characters } from '../types/api'
+import { RickMortyLocations, type RickMortyCharacters } from '../types/api'
 
 export function UseData () {
-
   const getCharacters = async () => {
     const res = await fetch('https://rickandmortyapi.com/api/character')
     const { results: character } = (await res.json()) as RickMortyCharacters
-    console.log("api    " + character)
     return character
   }
 
-  return { getCharacters }
+  const getLocations = async () => {
+    const res = await fetch('https://rickandmortyapi.com/api/location')
+    const { results: location } = (await res.json()) as RickMortyLocations
+    return location
+  }
+
+  return { getCharacters, getLocations }
 }
