ff33d844585c6b833abd4ef5befdca45c708d53b
BryanGordon <bryanbge92@gmail.com>
2024-06-17 20:07:13 -0500

    Add more routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,21 @@ import { Home } from './components/Home'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Zones } from './components/Zones'
 import { Login } from './components/Login'
+import { FavsContextProvider } from './context/FavsContext'
+import { FavoriteList } from './components/FavoriteList'
 
 function App () {
   return (
     <main>
       <Router>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/ubicaciones' element={<Zones />} />
-          <Route path='/login' element={<Login />} />
-        </Routes>
+        <FavsContextProvider>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/ubicaciones' element={<Zones />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/favoritos' element={<FavoriteList />} />
+          </Routes>
+        </FavsContextProvider>
       </Router>
     </main>
   )
