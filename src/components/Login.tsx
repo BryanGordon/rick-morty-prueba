2690905b1414e772467138a1121701b527456538
BryanGordon <bryanbge92@gmail.com>
2024-06-26 16:02:17 -0500

    Add login redirect

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,11 +2,13 @@ import { UserIcon } from '../icons/UserIcon'
 import { useState, useContext } from 'react'
 import { LoginContext } from '../context/LoginContext'
 import { Navbar } from './Navbar'
+import { useNavigate } from 'react-router-dom'
 
 export function Login () {
   const [user, setUser] = useState('')
   const [password, setPassword] = useState('')
   const context = useContext(LoginContext)
+  const navigate = useNavigate()
 
   const userPass = '#admin1'
   const passwordPass = '1234567890'
@@ -21,34 +23,48 @@ export function Login () {
 
   const handleLog = () => {
     if ((user === userPass) && (password === passwordPass)) {
-      console.log('antes   ' + logged)
+      alert('Se ha iniciado sesión.')
       setLogged(!logged)
-      console.log('despues   ' + logged)
+      navigate('/')
     } else {
       alert('Usuario no coincide')
     }
   }
-  return (
-    <section id='login-section'>
-      <Navbar />
-      <div className='icon-container'>
-        <UserIcon />
-      </div>
-      <div className='form-container'>
-        <form action='submit'>
-          <div className='data-section'>
-            <label className='input-title'>Ingrese el correo electronico:</label>
-            <input onChange={(e) => setUser(e.target.value)} type='email' placeholder='example@gmail.com' className='input-form' />
+
+  if (!logged) {
+    return (
+      <>
+        <header>
+          <Navbar />
+        </header>
+
+        <section id='login-section'>
+          <div className='icon-container'>
+            <UserIcon />
           </div>
-          <div className='data-section'>
-            <label className='input-title'>Ingrese la contraseña:</label>
-            <input onChange={(e) => setPassword(e.target.value)} type='password' placeholder='Contraseña' className='input-form' />
+          <div className='form-container'>
+            <form action='submit'>
+              <div className='data-section'>
+                <label className='input-title'>Ingrese el correo electronico:</label>
+                <input onChange={(e) => setUser(e.target.value)} type='email' placeholder='example@gmail.com' className='input-form' />
+              </div>
+              <div className='data-section'>
+                <label className='input-title'>Ingrese la contraseña:</label>
+                <input onChange={(e) => setPassword(e.target.value)} type='password' placeholder='Contraseña' className='input-form' />
+              </div>
+            </form>
           </div>
-        </form>
+          <div className='button-container'>
+            <button onClick={handleLog} className='form-button'>Ingresar</button>
+          </div>
+        </section>
+      </>
+    )
+  } else {
+    return (
+      <div>
+        <button>Cerrar cesion</button>
       </div>
-      <div className='button-container'>
-        <button onClick={handleLog} className='form-button'>Ingresar</button>
-      </div>
-    </section>
-  )
+    )
+  }
 }
