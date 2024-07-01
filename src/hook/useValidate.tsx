08feddd9f9f6406b38cc54f9fdd83eb9a81bd3ba
BryanGordon <bryanbge92@gmail.com>
2024-07-01 18:38:10 -0500

    Update custom hook

--- src/hook/useValidate.tsx
+++ src/hook/useValidate.tsx
@@ -1,21 +1,55 @@
 import { useContext } from "react";
 import { LoginContext } from "../context/LoginContext";
+import { FavsContext } from "../context/FavsContext";
+import { FavZonesContext } from "../context/FavsZonesContext";
+import { Error406 } from "../components/Error406";
 
 export function useValidate () {
-  const getValidationCharacters = () => {
-    const contextRes = useContext(LoginContext)
+  const contextCharacters = useContext(FavsContext)
+  const contextZones = useContext(FavZonesContext)
+  const contextLogin = useContext(LoginContext)
 
+  if ((contextCharacters === undefined) || (contextZones === undefined) || (contextLogin === undefined)) {
+    return (
+      <Error406 />
+    )
+  }
+  /*
+  const getValidationCharacters = () => {
+    const contextRes = useContext(FavsContext)
     if (contextRes === undefined) {
       return (
-        // Hacer un componente si ocurre undefinend (40X)
-        <div>
-          Loading...
-        </div>
+        <Error406 />
       )
     } else {
       return contextRes
     }
   }
 
-  return { getValidationCharacters }
+  const getValidationZones = () => {
+    const contextRes = useContext(FavZonesContext)
+
+    if (contextRes === undefined) {
+      return (
+        <Error406 />
+      )
+    } else {
+      return contextRes
+    }
+  }
+
+  const getValidationLogin = () => {
+    const contextRes = useContext(LoginContext)
+
+    if (contextRes === undefined) {
+      return (
+        <Error406 />
+      )
+    } else {
+      return contextRes
+    }
+  }
+  */
+
+  return { contextCharacters, contextZones, contextLogin }
 }
