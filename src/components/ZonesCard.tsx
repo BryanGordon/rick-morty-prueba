5e800fd21977cd2e94b4e78f1383dd8d97dcc73e
BryanGordon <bryanbge92@gmail.com>
2024-07-02 15:25:26 -0500

    Create zone card component

---
+++ src/components/ZonesCard.tsx
@@ -0,0 +1,25 @@
+import { Star } from "../icons/Star"
+import { Locations } from "../types/api"
+
+interface Props {
+  location: Locations
+}
+
+export function ZonesCard (zones: Props) {
+  return (
+    <article key={zones.location.id} className='zones-card'>
+      <h4>
+        {zones.location.name}
+        <span id='icon-star' onClick={() => handleFav(zones)}>
+          <Star />
+        </span>
+      </h4>
+      <div className='zone-info-container'>
+        <h5>Tipo: </h5>
+        <span>{zones.location.type}</span>
+        <h5>Dimensión: </h5>
+        <span>{zones.location.dimension}</span>
+      </div>
+    </article>
+  )
+}
