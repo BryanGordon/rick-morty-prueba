8f4cb4598cdc7b7bdfd76264f70c9edb3c0e8687
BryanGordon <bryanbge92@gmail.com>
2024-07-03 17:11:29 -0500

    Refactor locations

--- src/components/Zones.tsx
+++ src/components/Zones.tsx
@@ -1,32 +1,14 @@
-import { Star } from '../icons/Star'
-import { useContext, useEffect, useRef, useState } from 'react'
-import { FavZonesContext } from '../context/FavsZonesContext'
+import { useEffect, useRef, useState } from 'react'
 import { type Locations } from '../types/api'
 import { UseData } from '../hook/useData'
 import { Navbar } from './Navbar'
+import { ZonesCard } from './ZonesCard'
 
 export function Zones () {
-  const context = useContext(FavZonesContext)
   const { getLocations } = UseData()
   const originalZones = useRef<Locations[]>([])
   const [zones, setZones] = useState<Locations[]>([])
   const [currentPage, setCurrentPage] = useState(1)
-
-  if (!context) {
-    return (
-      <div>
-        Loading...
-      </div>
-    )
-  }
-
-  const { favZones, setFavZones } = context
-
-  const handleFav = (fav: Locations) => {
-    let favorites = [...favZones]
-    favorites.push(fav)
-    setFavZones(favorites)
-  }
 
   useEffect(() => {
     getLocations(currentPage)
@@ -50,33 +32,14 @@ export function Zones () {
 
       <div className='zones-container'>
         {
-          zones.map((location) => (
-            <article key={location.id} className='zones-card'>
-
-              <h4>
-                {location.name}
-                <span id='icon-star' onClick={() => handleFav(location)}>
-                  <Star />
-                </span>
-              </h4>
-              <div className='zone-info-container'>
-
-                <h5>Tipo: </h5>
-                <span>{location.type}</span>
-
-                <h5>Dimensión: </h5>
-                <span>{location.dimension}</span>
-
-              </div>
-
-            </article>
-          ))
+          zones.map((item) => <ZonesCard location={item} key={item.id} />)
         }
-
       </div>
+
       <div className='button-container'>
         <button className='see-more-button' onClick={() => setCurrentPage(currentPage + 1)}>Mostrar más zonas</button>
       </div>
+
     </section>
   )
 }
