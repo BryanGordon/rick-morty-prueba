4d3d152d351b651545df698fff9102260fdebfad
BryanGordon <bryanbge92@gmail.com>
2024-07-03 17:13:10 -0500

    Update zones card

--- src/components/ZonesCard.tsx
+++ src/components/ZonesCard.tsx
@@ -1,16 +1,30 @@
 import { Star } from "../icons/Star"
 import { Locations } from "../types/api"
+import { useValidate } from "../hook/useValidate"
+import { Error406 } from "./Error406"
 
 interface Props {
   location: Locations
 }
 
 export function ZonesCard (zones: Props) {
+  const { contextZones } = useValidate()
+
+  if (!contextZones) return <Error406 />
+
+  const { favZones, setFavZones } = contextZones
+
+  const handleFav = (fav: Locations) => {
+    let favorites = [...favZones]
+    favorites.push(fav)
+    setFavZones(favorites)
+  }
+
   return (
-    <article key={zones.location.id} className='zones-card'>
+    <article className='zones-card'>
       <h4>
         {zones.location.name}
-        <span id='icon-star' onClick={() => handleFav(zones)}>
+        <span id='icon-star' onClick={() => handleFav(zones.location)}>
           <Star />
         </span>
       </h4>
