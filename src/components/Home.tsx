e0068b47af53e99fe3b50fd656a6fc81b9fe3f70
BryanGordon <bryanbge92@gmail.com>
2024-07-03 17:11:05 -0500

    Home refactor

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,34 +1,14 @@
-import { Star } from '../icons/Star'
-import { useEffect, useRef, useState, useContext } from 'react'
-import { FavsContext } from '../context/FavsContext'
+import { useEffect, useRef, useState } from 'react'
 import { type Characters } from '../types/api'
 import { UseData } from '../hook/useData'
 import { Navbar } from './Navbar'
-import { useValidate } from '../hook/useValidate'
+import { CharactersCard } from './CharactersCard'
 
 export function Home () {
-  // const context = useContext(FavsContext)
   const { getCharacters } = UseData()
   const firstCharacters = useRef<Characters[]>([])
   const [character, setCharacters] = useState<Characters[]>([])
   const [currentPage, setCurrentPage] = useState(1)
-  const { contextCharacters } = useValidate()
-  /*
-  if (!context) {
-    return (
-      <div>
-        Loading...
-      </div>
-    )
-  }
-  */
-  const { favs, setFavs } = contextCharacters
-
-  const handleFavorites = (fav: Characters) => {
-    const aux = [...favs]
-    aux.push(fav)
-    setFavs(aux)
-  }
 
   useEffect(() => {
     getCharacters(currentPage)
@@ -50,29 +30,7 @@ export function Home () {
 
       <div className='players-container'>
         {
-          character.map((character) => (
-            <article key={character.id} className='player-card'>
-              <picture>
-                <img src={character.image} alt={character.name} />
-              </picture>
-
-              <h4>
-                {character.name}
-                <span id='icon-star' onClick={() => handleFavorites(character)}>
-                  <Star />
-                </span>
-              </h4>
-              <div className='pers-info-container'>
-                <h5>Especie: </h5>
-                <span>{character.species}</span>
-                <h5>Status: </h5>
-                <span>{character.status}</span>
-                <h5>Genero: </h5>
-                <span>{character.gender}</span>
-              </div>
-
-            </article>
-          ))
+          character.map((item) => <CharactersCard character={item} key={item.id} />)
         }
       </div>
       <div className='button-container'>
