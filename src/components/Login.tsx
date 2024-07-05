1cf5ffe7b115fc59fc8dd1fead4fdb8fabf31b17
BryanGordon <bryanbge92@gmail.com>
2024-07-05 18:34:43 -0500

    Refactor login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,38 +1,32 @@
 import { UserIcon } from '../icons/UserIcon'
-import { useState, useContext } from 'react'
-import { LoginContext } from '../context/LoginContext'
+import { useState } from 'react'
 import { Navbar } from './Navbar'
 import { useNavigate } from 'react-router-dom'
 import { Error406 } from './Error406'
+import { useValidate } from '../hook/useValidate'
+import { Signout } from './Signout'
 
 export function Login () {
   const [user, setUser] = useState('')
   const [password, setPassword] = useState('')
-  const context = useContext(LoginContext)
+  const { contextLogin } = useValidate()
   const navigate = useNavigate()
 
   const userPass = '#admin1'
   const passwordPass = '1234567890'
 
-  if (context === undefined) {
-    return (
-      <Error406 />
-    )
-  }
-
-  const { logged, setLogged } = context
+  if (!contextLogin) return <Error406 />
 
   const handleLog = () => {
     if ((user === userPass) && (password === passwordPass)) {
-      alert('Se ha iniciado sesión.')
-      setLogged(!logged)
+      contextLogin.setLogged(!contextLogin.logged)
       navigate('/')
     } else {
       alert('El usuario o contraseña no coinciden.')
     }
   }
 
-  if (!logged) {
+  if (!contextLogin.logged) {
     return (
       <>
         <header>
@@ -40,39 +34,38 @@ export function Login () {
         </header>
 
         <section id='login-section'>
+
           <div className='icon-container'>
             <UserIcon />
           </div>
+
           <div className='form-container'>
             <form action='submit'>
+
               <div className='data-section'>
                 <label className='input-title'>Ingrese el correo electronico:</label>
                 <input onChange={(e) => setUser(e.target.value)} type='email' placeholder='example@gmail.com' className='input-form' />
               </div>
+
               <div className='data-section'>
                 <label className='input-title'>Ingrese la contraseña:</label>
                 <input onChange={(e) => setPassword(e.target.value)} type='password' placeholder='Contraseña' className='input-form' />
               </div>
+
             </form>
           </div>
+
           <div className='button-container'>
-            <button onClick={handleLog} className='form-button'>Ingresar</button>
+            <button
+              onClick={handleLog}
+              className='form-button'
+            >
+              Ingresar
+            </button>
           </div>
+
         </section>
       </>
     )
-  } else {
-    return (
-      <>
-        <header>
-          <Navbar />
-        </header>
-
-        <div className='button-container'>
-          <button onClick={() => setLogged(!logged)} className='form-button'>Cerrar sesión</button>
-        </div>
-      </>
-
-    )
-  }
+  } else return <Signout />
 }
